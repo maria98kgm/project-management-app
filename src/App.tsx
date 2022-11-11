117ab62680e5def8761956165324488a21e4c15c
Tanya Samal <solesytto@gmail.com>
2022-11-11 16:29:41 +0300

    feat: add mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from './themes/theme';
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
---
+++ src/themes/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#6C3ED4',
+    },
+    secondary: {
+      main: '#BE92E8',
+    },
+    background: {
+      default: '#F1E6EA',
+    },
+  },
+  typography: {
+    fontFamily: 'Lato, sans-serif',
+  },
+});
