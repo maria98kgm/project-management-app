81fd5a1ac5103b4cca74eb3b0debceabb62f1b69
Tanya Samal <solesytto@gmail.com>
2022-11-12 12:37:02 +0300

    feat: first screen markup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { theme } from './themes/theme';
+import './styles/style.scss';
 
 export const App = () => {
   return (
--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,5 +1,29 @@
+import { FeatureCard } from '../../components/FeatureCard';
+import likeImg from '../../assets/main-page/like.svg';
 import './style.scss';
 
-export const Welcome = () => {
-  return <h1>Welcome</h1>;
-};
+export const Welcome = () => (
+  <>
+    <div className="banner-block">
+      <div className="container banner-content">
+        <div className="cta">
+          <h1>Make Business Flow Easyer With Us</h1>
+          <p>
+            Your amazing product deserves an easy way to manage it. Our app will provide you with a
+            nessesary instruments no matter what your idea is about.
+          </p>
+        </div>
+        <div className="mobiles"></div>
+      </div>
+    </div>
+    <div className="container feature-text">
+      <div className="feature-title">
+        <FeatureCard
+          imageUrl={likeImg}
+          title="A complete feature stack ready to help you"
+          description="This application will help you to achieve the set goals for an individual in a team or group of developers."
+        />
+      </div>
+    </div>
+  </>
+);
