81b728627c7dab5edfbab8c7ef3dc95c397afd5d
SaintSanta <aleks2art@gmail.com>
2022-11-12 03:59:51 +0300

    feat: add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,24 @@
 import './style.scss';
+import appLogo from '../../assets/img/app_logo.png';
 
 export const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <div className="header">
+        <img src={appLogo} />
+        <div className="control">
+          <div className="language">
+            <select>
+              <option>En</option>
+              <option>Ru</option>
+            </select>
+          </div>
+          <div className="authorization">
+            <button>Sign in</button>
+            <button>Sign up</button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
