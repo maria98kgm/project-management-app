519ddc82e54e39a103b1548b697fd069a9df0602
SaintSanta <aleks2art@gmail.com>
2022-11-13 23:39:55 +0300

    feat: add sticky header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,23 @@
 import './style.scss';
 import appLogo from '../../assets/img/app_logo.png';
+import { useState } from 'react';
 
 export const Header = () => {
+  const [header, setHeader] = useState(false);
+
+  const changeHeader = () => {
+    if (window.scrollY >= 80) {
+      setHeader(true);
+    } else {
+      setHeader(false);
+    }
+  };
+
+  window.addEventListener('scroll', changeHeader);
+
   return (
     <header>
-      <div className="header">
+      <div className={header ? 'header sticky' : 'header'}>
         <img src={appLogo} />
         <div className="control">
           <div className="language">
