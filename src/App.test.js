d86b94ac3dad8c875ac23ed43132fdda5a54850f
Tanya Samal <solesytto@gmail.com>
2022-11-16 09:16:17 +0300

    fix: test error

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Header } from './components/Header';
 
 test('renders element', () => {
-  render(<Header />);
-  expect(screen.getByText(/Sign up/i)).toBeInTheDocument();
+  render(
+    <Router>
+      <Header />
+    </Router>
+  );
+  expect(screen.getByText(/En/i)).toBeInTheDocument();
 });
