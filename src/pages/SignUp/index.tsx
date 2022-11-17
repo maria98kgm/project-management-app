4e7008f74cbdd74840f66355a3139d6bfceedaa4
Marie Kagermanova <maria.kgm04@gmail.com>
2022-11-17 04:09:04 +0300

    feat: rewrite form with MUI components

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,7 @@
 import './style.scss';
-import { ErrorMessage } from '@hookform/error-message';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { Button, TextField } from '@mui/material';
 import { AuthData, Paths } from '../../models';
 import { setCookie } from '../../share/setCookie';
 import { URL_BASE } from '../../constants';
@@ -86,81 +86,67 @@ export const SignUp = () => {
     <section className="signUp-content">
       <form onSubmit={handleSubmit(onSubmit)} className="signUp-form">
         <h2>Create an account</h2>
-        <label className="signUp-field-container">
-          <input
-            type="text"
-            {...register('userName', {
-              required: 'This field is required!',
-              minLength: { value: 2, message: 'Min length is 2!' },
-              maxLength: { value: 20, message: 'Max length is 20!' },
-            })}
-            autoComplete="nickname"
-            placeholder="Name*"
-            className="signUp-field"
-          />
-          <ErrorMessage
-            errors={errors}
-            name="userName"
-            render={({ message }) => <p className="errorMessage">{message}</p>}
-          />
-        </label>
-        <label className="signUp-field-container">
-          <input
-            type="text"
-            {...register('login', {
-              required: 'This field is required!',
-              minLength: { value: 2, message: 'Min length is 2!' },
-              maxLength: { value: 20, message: 'Max length is 20!' },
-            })}
-            autoComplete="username"
-            placeholder="Login*"
-            className="signUp-field"
-          />
-          <ErrorMessage
-            errors={errors}
-            name="login"
-            render={({ message }) => <p className="errorMessage">{message}</p>}
-          />
-        </label>
-        <label className="signUp-field-container">
-          <input
-            type="password"
-            {...register('password', {
-              required: 'This field is required!',
-              minLength: { value: 6, message: 'Min length is 6!' },
-              maxLength: { value: 20, message: 'Max length is 20!' },
-              validate: validatePassword,
-            })}
-            autoComplete="new-password"
-            placeholder="Password*"
-            className="signUp-field"
-          />
-          <ErrorMessage
-            errors={errors}
-            name="password"
-            render={({ message }) => <p className="errorMessage">{message}</p>}
-          />
-        </label>
-        <label className="signUp-field-container">
-          <input
-            type="password"
-            {...register('repeatPassword', {
-              required: 'This field is required!',
-              validate: validateRepeatPassword,
-            })}
-            autoComplete="new-password"
-            placeholder="Repeat Password*"
-            className="signUp-field"
-          />
-          <ErrorMessage
-            errors={errors}
-            name="repeatPassword"
-            render={({ message }) => <p className="errorMessage">{message}</p>}
-          />
-        </label>
-        <button type="submit" className="signUp-button">
+        <TextField
+          {...register('userName', {
+            required: 'This field is required!',
+            minLength: { value: 2, message: 'Min length is 2!' },
+            maxLength: { value: 20, message: 'Max length is 20!' },
+          })}
+          label="Name"
+          variant="standard"
+          error={!!getFieldState('userName').error}
+          helperText={errors['userName']?.message}
+          autoComplete="nickname"
+          required
+          fullWidth
+        />
+        <TextField
+          {...register('login', {
+            required: 'This field is required!',
+            minLength: { value: 2, message: 'Min length is 2!' },
+            maxLength: { value: 20, message: 'Max length is 20!' },
+          })}
+          label="Login"
+          variant="standard"
+          error={!!getFieldState('login').error}
+          helperText={errors['login']?.message}
+          autoComplete="username"
+          required
+          fullWidth
+        />
+        <TextField
+          {...register('password', {
+            required: 'This field is required!',
+            minLength: { value: 6, message: 'Min length is 6!' },
+            maxLength: { value: 20, message: 'Max length is 20!' },
+            validate: validatePassword,
+          })}
+          label="Password"
+          variant="standard"
+          error={!!getFieldState('password').error}
+          helperText={errors['password']?.message}
+          type="password"
+          autoComplete="new-password"
+          required
+          fullWidth
+        />
+        <TextField
+          {...register('repeatPassword', {
+            required: 'This field is required!',
+            validate: validateRepeatPassword,
+          })}
+          label="Repeat Password"
+          variant="standard"
+          error={!!getFieldState('repeatPassword').error}
+          helperText={errors['repeatPassword']?.message}
+          type="password"
+          autoComplete="new-password"
+          required
+          fullWidth
+        />
+        <Button variant="contained" type="submit">
           Sign Up
-        </button>
+        </Button>
       </form>
     </section>
   );
