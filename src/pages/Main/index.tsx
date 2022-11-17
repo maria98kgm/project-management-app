2e795c1bfcad7b27dd14ebaf267735aee7db4a3b
Marie Kagermanova <maria.kgm04@gmail.com>
2022-11-17 04:31:17 +0300

    feat: add loupe to board item

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import './style.scss';
 import { ReactComponent as EditPencil } from '../../assets/editPencil.svg';
 import { ReactComponent as TrashCan } from '../../assets/trashCan.svg';
+import { ReactComponent as Loupe } from '../../assets/loupe.svg';
 
 export const Main = () => {
   return (
@@ -23,6 +24,7 @@ export const Main = () => {
             </p>
             <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
           </div>
+          <Loupe className="boardItem-loupe" />
         </div>
         <div className="boardItem">
           <div className="boardItem-top">
@@ -39,6 +41,7 @@ export const Main = () => {
             </p>
             <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
           </div>
+          <Loupe className="boardItem-loupe" />
         </div>
         <div className="boardItem">
           <div className="boardItem-top">
@@ -55,6 +58,7 @@ export const Main = () => {
             </p>
             <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
           </div>
+          <Loupe className="boardItem-loupe" />
         </div>
         <div className="boardItem">
           <div className="boardItem-top">
@@ -71,6 +75,7 @@ export const Main = () => {
             </p>
             <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
           </div>
+          <Loupe className="boardItem-loupe" />
         </div>
         <div className="boardItem">
           <div className="boardItem-top">
@@ -87,6 +92,7 @@ export const Main = () => {
             </p>
             <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
           </div>
+          <Loupe className="boardItem-loupe" />
         </div>
         <div className="boardItem">
           <div className="boardItem-top">
@@ -100,58 +106,7 @@ export const Main = () => {
             <p className="boardItem-description">Lorem ipsum dolor sit amet</p>
             <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
           </div>
-        </div>
-        <div className="boardItem">
-          <div className="boardItem-top">
-            <h2 className="boardItem-title">Title</h2>
-            <div className="boardItem-tools">
-              <EditPencil className="boardItem-icon" />
-              <TrashCan className="boardItem-icon" />
-            </div>
-          </div>
-          <div className="boardItem-bottom">
-            <p className="boardItem-description">Lorem ipsum dolor sit amet</p>
-            <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
-          </div>
-        </div>
-        <div className="boardItem">
-          <div className="boardItem-top">
-            <h2 className="boardItem-title">Title</h2>
-            <div className="boardItem-tools">
-              <EditPencil className="boardItem-icon" />
-              <TrashCan className="boardItem-icon" />
-            </div>
-          </div>
-          <div className="boardItem-bottom">
-            <p className="boardItem-description">Lorem ipsum dolor sit amet</p>
-            <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
-          </div>
-        </div>
-        <div className="boardItem">
-          <div className="boardItem-top">
-            <h2 className="boardItem-title">Title</h2>
-            <div className="boardItem-tools">
-              <EditPencil className="boardItem-icon" />
-              <TrashCan className="boardItem-icon" />
-            </div>
-          </div>
-          <div className="boardItem-bottom">
-            <p className="boardItem-description">Lorem ipsum dolor sit amet</p>
-            <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
-          </div>
-        </div>
-        <div className="boardItem">
-          <div className="boardItem-top">
-            <h2 className="boardItem-title">Title</h2>
-            <div className="boardItem-tools">
-              <EditPencil className="boardItem-icon" />
-              <TrashCan className="boardItem-icon" />
-            </div>
-          </div>
-          <div className="boardItem-bottom">
-            <p className="boardItem-description">Lorem ipsum dolor sit amet</p>
-            <p className="boardItem-columns-tasks">Columns: 4, Tasks: 10</p>
-          </div>
+          <Loupe className="boardItem-loupe" />
         </div>
       </div>
     </section>
