04b2971e088c9985d727620fa84840a4b70c06d4
Tanya Samal <solesytto@gmail.com>
2022-11-20 22:28:25 +0300

    feat: edd editable title

--- src/components/BoardColumn/index.tsx
+++ src/components/BoardColumn/index.tsx
@@ -1,22 +1,55 @@
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
-import BorderColorIcon from '@mui/icons-material/BorderColor';
-import { Column } from '../../models/Board.interface';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { AlertColor } from '@mui/material/Alert';
 import { BoardTask } from '../BoardTask';
+import { Column } from '../../models/Board.interface';
+import { Toast } from '../Toast';
 import './style.scss';
 
 export const BoardColumn: React.FC<{ column: Partial<Column> }> = ({ column }) => {
   const { t } = useTranslation();
+  const [isEdit, setIsEdit] = useState(false);
+  const [toastState, setToastState] = useState({
+    isOpen: false,
+    severity: 'info' as AlertColor,
+    message: '',
+  });
 
   return (
     <div className="boardColumn">
       <div className="column-title">
-        <div className="title">{column.title}</div>
-        <div className="buttons">
-          <BorderColorIcon color="info" />
-          <DeleteIcon color="info" />
-        </div>
+        {isEdit ? (
+          <React.Fragment>
+            <input value={column.title} />
+            <CheckCircleOutlineIcon
+              color="info"
+              onClick={() => {
+                setIsEdit(false);
+                setToastState({ isOpen: true, message: t('INFO.APPLIED'), severity: 'success' });
+              }}
+            />
+            <HighlightOffIcon
+              color="info"
+              onClick={() => {
+                setIsEdit(false);
+                setToastState({ isOpen: true, message: t('INFO.CANCELLED'), severity: 'warning' });
+              }}
+            />
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div className="title" onClick={() => setIsEdit(true)}>
+              {column.title}
+            </div>
+            <div className="buttons">
+              <DeleteIcon color="info" />
+            </div>
+          </React.Fragment>
+        )}
       </div>
       <div className="tasks">
         {column.tasks && column.tasks.length !== 0 ? (
@@ -35,6 +68,12 @@ export const BoardColumn: React.FC<{ column: Partial<Column> }> = ({ column }) =
       <Button color="primary" startIcon={'+'}>
         {t('BUTTONS.ADD_TASK')}
       </Button>
+      <Toast
+        isOpen={toastState.isOpen}
+        severity={toastState.severity}
+        message={toastState.message}
+        handleClose={() => setToastState({ ...toastState, isOpen: false })}
+      />
     </div>
   );
 };
