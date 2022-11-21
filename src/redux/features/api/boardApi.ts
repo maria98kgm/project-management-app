c37f0522aae281d81905921b70e23cd5b96c0c09
Marie Kagermanova <maria.kgm04@gmail.com>
2022-11-21 18:34:49 +0300

    feat: edit endpoints for authApi and userApi; add boardApi; rewrite forms with RTK Query

---
+++ src/redux/features/api/boardApi.ts
@@ -0,0 +1,94 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { URL_BASE } from '../../../constants';
+import { getCookieToken } from '../../../share/cookieToken';
+
+interface BoardData {
+  _id?: string;
+  title: string;
+  owner: string;
+  users: string[];
+}
+
+export const boardApi = createApi({
+  reducerPath: 'boardApi',
+  baseQuery: fetchBaseQuery({ baseUrl: `${URL_BASE}/` }),
+  endpoints: (builder) => ({
+    getAllBoards: builder.mutation<BoardData[], null>({
+      query() {
+        return {
+          url: 'boards',
+          headers: {
+            Authorization: getCookieToken(),
+          },
+        };
+      },
+    }),
+    createBoard: builder.mutation<BoardData, BoardData>({
+      query(data) {
+        return {
+          url: 'boards',
+          method: 'POST',
+          headers: {
+            Authorization: getCookieToken(),
+          },
+          body: data,
+        };
+      },
+    }),
+    getBoard: builder.mutation<BoardData, string>({
+      query(boardId) {
+        return {
+          url: `boards/${boardId}`,
+          headers: {
+            Authorization: getCookieToken(),
+          },
+        };
+      },
+    }),
+    updateBoard: builder.mutation<BoardData, { boardId: string; boardInfo: BoardData }>({
+      query(data) {
+        return {
+          url: `boards/${data.boardId}`,
+          method: 'PUT',
+          headers: {
+            Authorization: getCookieToken(),
+          },
+          body: data.boardInfo,
+        };
+      },
+    }),
+    deleteBoard: builder.mutation<BoardData, string>({
+      query(boardId) {
+        return {
+          url: `boards/${boardId}`,
+          method: 'DELETE',
+          headers: {
+            Authorization: getCookieToken(),
+          },
+        };
+      },
+    }),
+    getBoardsByIdsList: builder.mutation<BoardData[], string[]>({
+      query(idsList) {
+        return {
+          url: `boardsSet/${idsList}`,
+          headers: {
+            Authorization: getCookieToken(),
+          },
+        };
+      },
+    }),
+    getAllUserBoards: builder.mutation<BoardData, string>({
+      query(userId) {
+        return {
+          url: `boardsSet/${userId}`,
+          headers: {
+            Authorization: getCookieToken(),
+          },
+        };
+      },
+    }),
+  }),
+});
+
+export const {} = boardApi;
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from './features/api/authApi';
 import { userApi } from './features/api/userApi';
+import { boardApi } from './features/api/boardApi';
 import userReducer from './features/userSlice';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
-    userState: userReducer,
+    [boardApi.reducerPath]: boardApi.reducer,
+    user: userReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({}).concat([authApi.middleware, userApi.middleware]),
+    getDefaultMiddleware({}).concat([authApi.middleware, userApi.middleware, boardApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
