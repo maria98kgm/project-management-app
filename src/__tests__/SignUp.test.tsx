7654cc88ea84941d6ba30dfab6e08483514c921d
Tanya Samal <solesytto@gmail.com>
2022-11-23 09:18:34 +0300

    fix: test with mocked store

--- src/__tests__/SignUp.test.tsx
+++ src/__tests__/SignUp.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import { SignUp } from '../pages/SignUp';
 
 describe('Sign Up', () => {
+  const initialState = { output: 10 };
+  const mockStore = configureStore();
+  const store = mockStore(initialState);
+
   test('Test for form presence', () => {
     render(
       <Router>
-        <SignUp />
+        <Provider store={store}>
+          <SignUp />
+        </Provider>
       </Router>
     );
 
