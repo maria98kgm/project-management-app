c2895418d0a178f79a3d71320be61c2afb75a20d
Tanya Samal <solesytto@gmail.com>
2022-11-24 14:15:50 +0300

    fix: test error

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Header } from '../components/Header';
+import { store } from '../redux/store';
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: (key: string) => key }),
@@ -9,7 +11,9 @@ jest.mock('react-i18next', () => ({
 test('renders element', () => {
   render(
     <Router>
-      <Header />
+      <Provider store={store}>
+        <Header />
+      </Provider>
     </Router>
   );
   expect(screen.getByText(/En/i)).toBeInTheDocument();
