bbea19806e73fb1a86eb5afef6c4588dee252a22
SaintSanta <aleks2art@gmail.com>
2022-12-04 11:57:17 +0300

    feat: hide buttons welcome page

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -63,152 +63,197 @@ export const NavBar: React.FC<NavBarProps> = ({ isToken, showBurger, createNewBo
     createNewBoard();
   };
 
-  if (!isToken) {
-    return (
+  return (
+    <React.Fragment>
       <nav className="control">
-        {showBurger ? (
+        {!isToken ? (
           <React.Fragment>
-            <IconButton onClick={toggleDrawer(true)} sx={{ p: 0 }}>
-              <MenuIcon />
-            </IconButton>
-            <Drawer
-              anchor="right"
-              variant="temporary"
-              open={openBurgerMenu}
-              onClose={toggleDrawer(false)}
-              PaperProps={{
-                sx: {
-                  width: 240,
-                  height: 260,
-                  borderRadius: '0 0 0 4px',
-                  background: 'transparent',
-                },
-              }}
-            >
-              <Box
-                sx={{
-                  height: 1,
-                  background:
-                    'linear-gradient(140.77deg, #be92e8 -20.7%, #6c3ed4 46.22%, #261436 100.74%);',
-                }}
-              >
-                <IconButton sx={{ ml: 22, mt: 1 }} onClick={toggleDrawer(false)}>
-                  <CloseIcon />
+            {showBurger ? (
+              <React.Fragment>
+                <IconButton onClick={toggleDrawer(true)} sx={{ p: 0 }}>
+                  <MenuIcon />
                 </IconButton>
-                <Divider sx={{ m: 2 }} />
-                <Box>
-                  <ListItemButton sx={menuButtonStyle}>
-                    <Button sx={{ color: 'white' }} onClick={() => handleMenuButton(Paths.SIGNUP)}>
-                      {t('BUTTONS.SIGNUP')}
-                    </Button>
-                  </ListItemButton>
-                  <ListItemButton sx={menuButtonStyle}>
-                    <Button sx={{ color: 'white' }} onClick={() => handleMenuButton(Paths.SIGNIN)}>
-                      {t('BUTTONS.SIGNIN')}
-                    </Button>
-                  </ListItemButton>
-                  <ListItemButton sx={menuButtonStyle}>
-                    <LanguageSwitch />
-                  </ListItemButton>
-                </Box>
-              </Box>
-            </Drawer>
+                <Drawer
+                  anchor="right"
+                  variant="temporary"
+                  open={openBurgerMenu}
+                  onClose={toggleDrawer(false)}
+                  PaperProps={{
+                    sx: {
+                      width: 240,
+                      height: 260,
+                      borderRadius: '0 0 0 4px',
+                      background: 'transparent',
+                    },
+                  }}
+                >
+                  <Box
+                    sx={{
+                      height: 1,
+                      background:
+                        'linear-gradient(140.77deg, #be92e8 -20.7%, #6c3ed4 46.22%, #261436 100.74%);',
+                    }}
+                  >
+                    <IconButton sx={{ ml: 22, mt: 1 }} onClick={toggleDrawer(false)}>
+                      <CloseIcon />
+                    </IconButton>
+                    <Divider sx={{ m: 2 }} />
+                    <Box>
+                      <ListItemButton sx={menuButtonStyle}>
+                        <Button
+                          sx={{ color: 'white' }}
+                          onClick={() => handleMenuButton(Paths.SIGNUP)}
+                        >
+                          {t('BUTTONS.SIGNUP')}
+                        </Button>
+                      </ListItemButton>
+                      <ListItemButton sx={menuButtonStyle}>
+                        <Button
+                          sx={{ color: 'white' }}
+                          onClick={() => handleMenuButton(Paths.SIGNIN)}
+                        >
+                          {t('BUTTONS.SIGNIN')}
+                        </Button>
+                      </ListItemButton>
+                      <ListItemButton sx={menuButtonStyle}>
+                        <LanguageSwitch />
+                      </ListItemButton>
+                    </Box>
+                  </Box>
+                </Drawer>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <Button variant="outlined" color="secondary" onClick={() => navigate(Paths.SIGNIN)}>
+                  {t('BUTTONS.SIGNIN')}
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => navigate(Paths.SIGNUP)}
+                >
+                  {t('BUTTONS.SIGNUP')}
+                </Button>
+                <LanguageSwitch />
+              </React.Fragment>
+            )}
           </React.Fragment>
         ) : (
           <React.Fragment>
-            <Button variant="outlined" color="secondary" onClick={() => navigate(Paths.SIGNIN)}>
-              {t('BUTTONS.SIGNIN')}
-            </Button>
-            <Button variant="contained" color="secondary" onClick={() => navigate(Paths.SIGNUP)}>
-              {t('BUTTONS.SIGNUP')}
-            </Button>
-            <LanguageSwitch />
+            {showBurger ? (
+              <React.Fragment>
+                <IconButton onClick={toggleDrawer(true)} sx={{ p: 0 }}>
+                  <MenuIcon />
+                </IconButton>
+                <Drawer
+                  anchor="right"
+                  variant="temporary"
+                  open={openBurgerMenu}
+                  onClose={toggleDrawer(false)}
+                  PaperProps={{
+                    sx: {
+                      width: 240,
+                      height: 380,
+                      borderRadius: '0 0 0 4px',
+                      background: 'transparent',
+                    },
+                  }}
+                >
+                  <Box
+                    sx={{
+                      height: 1,
+                      background:
+                        'linear-gradient(140.77deg, #be92e8 -20.7%, #6c3ed4 46.22%, #261436 100.74%);',
+                    }}
+                  >
+                    <IconButton sx={{ ml: 22, mt: 1 }} onClick={toggleDrawer(false)}>
+                      <CloseIcon />
+                    </IconButton>
+                    <Divider sx={{ m: 2 }} />
+                    <Box>
+                      {location.pathname === Paths.WELCOME ? (
+                        <React.Fragment>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <Button
+                              sx={{ color: 'white' }}
+                              onClick={() => handleMenuButton(Paths.MAIN)}
+                            >
+                              {t('BUTTONS.MAIN')}
+                            </Button>
+                          </ListItemButton>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <Button sx={{ color: 'white' }} onClick={signOut}>
+                              {t('BUTTONS.SIGNOUT')}
+                            </Button>
+                          </ListItemButton>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <LanguageSwitch />
+                          </ListItemButton>
+                        </React.Fragment>
+                      ) : (
+                        <React.Fragment>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <Button
+                              sx={{ color: 'white' }}
+                              onClick={() => handleMenuButton(Paths.MAIN)}
+                            >
+                              {t('BUTTONS.MAIN')}
+                            </Button>
+                          </ListItemButton>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <Button sx={{ color: 'white' }} onClick={createBoard}>
+                              {t('BUTTONS.NEWBOARD')}
+                            </Button>
+                          </ListItemButton>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <Button
+                              sx={{ color: 'white' }}
+                              onClick={() => handleMenuButton(Paths.EDITPROFILE)}
+                            >
+                              {t('BUTTONS.EDITPROFILE')}
+                            </Button>
+                          </ListItemButton>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <Button sx={{ color: 'white' }} onClick={signOut}>
+                              {t('BUTTONS.SIGNOUT')}
+                            </Button>
+                          </ListItemButton>
+                          <ListItemButton sx={menuButtonStyle}>
+                            <LanguageSwitch />
+                          </ListItemButton>
+                        </React.Fragment>
+                      )}
+                    </Box>
+                  </Box>
+                </Drawer>
+              </React.Fragment>
+            ) : location.pathname === Paths.WELCOME ? (
+              <React.Fragment>
+                <Button variant="contained" onClick={() => navigate(Paths.MAIN)}>
+                  {t('BUTTONS.MAIN')}
+                </Button>
+                <LanguageSwitch />
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <Button variant="contained" onClick={createBoard}>
+                  {t('BUTTONS.NEWBOARD')}
+                </Button>
+                <Button variant="contained" onClick={() => navigate(Paths.EDITPROFILE)}>
+                  {t('BUTTONS.EDITPROFILE')}
+                </Button>
+                <Button variant="contained" onClick={signOut}>
+                  {t('BUTTONS.SIGNOUT')}
+                </Button>
+                <Button variant="contained" onClick={() => navigate(Paths.MAIN)}>
+                  {t('BUTTONS.MAIN')}
+                </Button>
+                <LanguageSwitch />
+              </React.Fragment>
+            )}
           </React.Fragment>
         )}
       </nav>
-    );
-  }
-
-  return (
-    <nav className="control">
-      {showBurger ? (
-        <React.Fragment>
-          <IconButton onClick={toggleDrawer(true)} sx={{ p: 0 }}>
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            anchor="right"
-            variant="temporary"
-            open={openBurgerMenu}
-            onClose={toggleDrawer(false)}
-            PaperProps={{
-              sx: {
-                width: 240,
-                height: 380,
-                borderRadius: '0 0 0 4px',
-                background: 'transparent',
-              },
-            }}
-          >
-            <Box
-              sx={{
-                height: 1,
-                background:
-                  'linear-gradient(140.77deg, #be92e8 -20.7%, #6c3ed4 46.22%, #261436 100.74%);',
-              }}
-            >
-              <IconButton sx={{ ml: 22, mt: 1 }} onClick={toggleDrawer(false)}>
-                <CloseIcon />
-              </IconButton>
-              <Divider sx={{ m: 2 }} />
-              <Box>
-                <ListItemButton sx={menuButtonStyle}>
-                  <Button sx={{ color: 'white' }} onClick={() => handleMenuButton(Paths.MAIN)}>
-                    {t('BUTTONS.MAIN')}
-                  </Button>
-                </ListItemButton>
-                <ListItemButton sx={menuButtonStyle}>
-                  <Button sx={{ color: 'white' }} onClick={createBoard}>
-                    {t('BUTTONS.NEWBOARD')}
-                  </Button>
-                </ListItemButton>
-                <ListItemButton sx={menuButtonStyle}>
-                  <Button
-                    sx={{ color: 'white' }}
-                    onClick={() => handleMenuButton(Paths.EDITPROFILE)}
-                  >
-                    {t('BUTTONS.EDITPROFILE')}
-                  </Button>
-                </ListItemButton>
-                <ListItemButton sx={menuButtonStyle}>
-                  <Button sx={{ color: 'white' }} onClick={signOut}>
-                    {t('BUTTONS.SIGNOUT')}
-                  </Button>
-                </ListItemButton>
-                <ListItemButton sx={menuButtonStyle}>
-                  <LanguageSwitch />
-                </ListItemButton>
-              </Box>
-            </Box>
-          </Drawer>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Button variant="contained" onClick={createBoard}>
-            {t('BUTTONS.NEWBOARD')}
-          </Button>
-          <Button variant="contained" onClick={() => navigate(Paths.EDITPROFILE)}>
-            {t('BUTTONS.EDITPROFILE')}
-          </Button>
-          <Button variant="contained" onClick={signOut}>
-            {t('BUTTONS.SIGNOUT')}
-          </Button>
-          <Button variant="contained" onClick={() => navigate(Paths.MAIN)}>
-            {t('BUTTONS.MAIN')}
-          </Button>
-          <LanguageSwitch />
-        </React.Fragment>
-      )}
-    </nav>
+    </React.Fragment>
   );
 };
